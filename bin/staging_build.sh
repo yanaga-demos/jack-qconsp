6e8ca81eb8c8d13bef4dd38300b81a5a487d4b40
Edson Yanaga <edson@yanaga.com.br>
2014-04-11 13:49:58 -0300

    Adicionado -i ao ssh do staging.

--- bin/staging_build.sh
+++ bin/staging_build.sh
@@ -1,5 +1,5 @@
 rsync -avzP target/qconsp.war staging.insula.com.br:
-ssh -o StrictHostKeyChecking=no staging.insula.com.br << EOF
+ssh -i /home/ubuntu/.ssh/id_rsa -o StrictHostKeyChecking=no staging.insula.com.br << EOF
 sudo service tomcat7 stop
 sudo rm -fR /var/lib/tomcat7/webapps/ROOT*
 sudo cp qconsp.war /var/lib/tomcat7/webapps/ROOT.war
