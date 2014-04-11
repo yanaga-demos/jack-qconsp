69412944cb4e7292794ccf204197c3d2a949b973
Edson Yanaga <edson@yanaga.com.br>
2014-04-11 13:37:11 -0300

    Adicionados scripts de production e staging para o Go.

---
+++ bin/production.sh
@@ -0,0 +1,8 @@
+rsync -avzP target/qconsp.war production.insula.com.br:
+ssh staging.insula.com.br << EOF
+sudo service tomcat7 stop
+sudo rm -fR /var/lib/tomcat7/webapps/ROOT*
+sudo cp qconsp.war /var/lib/tomcat7/webapps/ROOT.war
+sudo service tomcat7 start
+sleep 5
+EOF
