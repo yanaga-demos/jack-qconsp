001b066d286e6849e2da0ac4ada105c47306f1d9
Edson Yanaga <edson@yanaga.com.br>
2014-04-11 13:43:59 -0300

    Adicionado -o StrictHostKeyChecking=no em production e staging para o Go.

--- bin/staging_build.sh
+++ bin/staging_build.sh
@@ -1,5 +1,5 @@
 rsync -avzP target/qconsp.war staging.insula.com.br:
-ssh staging.insula.com.br << EOF
+ssh -o StrictHostKeyChecking=no staging.insula.com.br << EOF
 sudo service tomcat7 stop
 sudo rm -fR /var/lib/tomcat7/webapps/ROOT*
 sudo cp qconsp.war /var/lib/tomcat7/webapps/ROOT.war
