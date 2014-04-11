a97caa333e65d438b9681903707a39afb5154a5d
Edson Yanaga <edson@yanaga.com.br>
2014-04-11 13:58:00 -0300

    Corrigido usuário em production e staging.

--- bin/staging_build.sh
+++ bin/staging_build.sh
@@ -1,6 +1,6 @@
 rsync -avzP target/qconsp.war staging.insula.com.br:
 whoami
-ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no staging.insula.com.br << EOF
+ssh -o StrictHostKeyChecking=no ubuntu@staging.insula.com.br << EOF
 sudo service tomcat7 stop
 sudo rm -fR /var/lib/tomcat7/webapps/ROOT*
 sudo cp qconsp.war /var/lib/tomcat7/webapps/ROOT.war
